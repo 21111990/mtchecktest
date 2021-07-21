510eb0817b1c2f332a6bc6471a3490ac73a11fa0
21111990 <87606783+21111990@users.noreply.github.com>
2021-07-21 17:35:33 +0530

    mtchecktest

--- mtchecktest/shopdummy/urls.py
+++ mtchecktest/shopdummy/urls.py
@@ -23,7 +23,7 @@ from django.views.static import serve
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('MTK/', include('products1.urls')),
+    path('', include('products1.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
